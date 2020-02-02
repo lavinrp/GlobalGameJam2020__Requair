c4e5814f032f926568f86a2d1cddb6ae9aafc829
gyurgyma <thetemplarofanubis@gmail.com>
2020-02-02 08:35:44 -0500

    testing

--- RequairSln/RequairLib/include/Requair/Core/Regions/HeroRegion.h
+++ RequairSln/RequairLib/include/Requair/Core/Regions/HeroRegion.h
@@ -10,6 +10,7 @@
 
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 //#include <utility>
 //#include <vector>
@@ -21,7 +22,7 @@ namespace REQ
     class libRequair HeroRegion : public TemplateRegion
     {
     public:
-        HeroRegion(const std::string& jsonFile);
+        HeroRegion(const std::string& jsonFile, sf::RenderWindow& window);
         HeroRegion() = delete;
 		void HandleEvent(sf::Event& even) override;
 
@@ -30,6 +31,7 @@ namespace REQ
     private:
         // Hero
         GB::AnimatedSprite m_hero;
+        GB::AnimatedSprite m_pot;
         std::unique_ptr<MoveAction> m_action;
 
         // ActionSet = Start Point, End Point, Action, Other Sprite/Object that moves?
@@ -37,6 +39,7 @@ namespace REQ
         std::vector<ActionSet> m_coordinates;*/
 
 
+        sf::RenderWindow& m_window;
         sf::Texture spriteSheet;
     };
 }
--- RequairSln/RequairLib/source/Core/Regions/HeroRegion.cpp
+++ RequairSln/RequairLib/source/Core/Regions/HeroRegion.cpp
@@ -5,6 +5,8 @@
 
 #include <GameBackbone/Core/UniformAnimationSet.h>
 
+#include <SFML/Graphics/RenderWindow.hpp>
+
 #include <memory>
 #include <iostream>
 
@@ -12,9 +14,9 @@ const int GridSize = 128;
 
 using namespace REQ;
 
-HeroRegion::HeroRegion(const std::string& jsonFile) : TemplateRegion()
+HeroRegion::HeroRegion(const std::string& jsonFile, sf::RenderWindow& window) : TemplateRegion(), m_window(window)
 {
-	spriteSheet.loadFromFile(R"(Textures/boss_man_walk.png)");
+	spriteSheet.loadFromFile(R"(Textures/Hero Boi/hero boi walk.png)");
 
 	GB::UniformAnimationSet::Ptr uniformAnimationSet = std::make_shared<GB::UniformAnimationSet>(sf::Vector2i(GridSize, GridSize));
 	// Add an animation to the UniformAnimationSet
@@ -27,20 +29,26 @@ HeroRegion::HeroRegion(const std::string& jsonFile) : TemplateRegion()
 	// Create an AnimatedSprite to display the UniformAnimation
 	m_hero.setTexture(spriteSheet);
 	m_hero.setAnimations(uniformAnimationSet);
+	m_pot.setTexture(spriteSheet);
+	m_pot.setAnimations(uniformAnimationSet);
 
-	// Move the AnimatedSprite
-	m_hero.move(150.0f, 0.0f);
+	m_hero.setPosition(10 * 128, 3 * 128);
+	m_pot.setPosition(10 * 128, 3 * 128);
+
 	// Tell the AnimatedSprite to update every one second
 	m_hero.setAnimationDelay(sf::seconds(0.3));
 	// Tell the AnimatedSprite to begin running its second animation
 	// The animation will reverse directions every time it reaches the beginning or end
 	m_hero.runAnimation(0, GB::ANIMATION_END_TYPE::ANIMATION_LOOP);
+	m_pot.runAnimation(0, GB::ANIMATION_END_TYPE::ANIMATION_LOOP);
 
 	// Tell the region to draw the AnimatedSprite
 	addDrawable(1, &m_hero);
+	addDrawable(1, &m_pot);
 	
 
-	m_action = std::make_unique<MoveAction>(m_hero, sf::Vector2f{ 150.0f, 550.0f });
+	m_action = std::make_unique<MoveAction>(m_hero, sf::Vector2f{ 7*128, 3 * 128 });
+	m_action->Then(std::make_unique<MoveAction>(m_hero, sf::Vector2f{ 10 * 128, 3 * 128 }));
 
 	/*MoveAction banana{ m_hero, sf::Vector2f{ 0.0f, 0.0f }, sf::Vector2f{ 10.0f, 10.0f } };
 	banana.Then(MoveAction{ GB::AnimatedSprite{}, sf::Vector2f{ 10.0f, 10.0f } })
@@ -52,8 +60,10 @@ HeroRegion::HeroRegion(const std::string& jsonFile) : TemplateRegion()
 void HeroRegion::update(sf::Int64 elapsedTime)
 {
 	m_action->update(elapsedTime);
-	//std::cout << m_hero.getPosition().x << " " << m_hero.getPosition().y << "\n";
-	std::cout << getDrawableCount()<< "\n";
+
+	sf::View view = m_window.getView();
+	view.setCenter(m_hero.getPosition());
+	m_window.setView(view);
 }
 
 void HeroRegion::HandleEvent(sf::Event& event) {
